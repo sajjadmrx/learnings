bad770c0e26b17eb069e8c85835479c694c2f47b
sajjadmrx <sajjadzibaee102@gmail.com>
2023-01-23 18:16:39 +0330

    silce

--- 06-arrays/main.go
+++ 06-arrays/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var pl = fmt.Println
 
-func main() {
+func main1() {
 	var userIds [2]int
 	userIds[0] = 1
 	userIds[1] = 2
@@ -18,4 +18,23 @@ func main() {
 	roles = append(roles, "ADMIN", "USER")
 	pl("\n")
 	pl(len(roles))
+
+}
+
+func main2() {
+	a := [...]int{1, 2, 3}
+	b := &a
+	b[1] = 4
+
+	pl(b, a)
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	b := a[:]
+	c := a[3:]
+	d := a[3:6]
+	pl(b)
+	pl(c)
+	pl(d)
 }
